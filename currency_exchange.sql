cf6054b521f9fdc9742ea52d7d0356ae138f94ad
vhinz2145 <130061358+vhinz2145@users.noreply.github.com>
2025-03-27 14:34:09 +0800

    Database

---
+++ currency_exchange.sql
@@ -0,0 +1,157 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 27, 2025 at 07:33 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `currency_exchange`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exchange_rates`
+--
+
+CREATE TABLE `exchange_rates` (
+  `id` bigint(20) NOT NULL,
+  `cut_off_date` date NOT NULL,
+  `exchange_rate` decimal(38,2) NOT NULL,
+  `source_currency` varchar(255) NOT NULL,
+  `target_currency` varchar(255) NOT NULL,
+  `version` int(11) DEFAULT NULL,
+  `unique_key` varchar(255) DEFAULT NULL,
+  `base_currency` varchar(255) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `rate` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `exchange_rates`
+--
+
+INSERT INTO `exchange_rates` (`id`, `cut_off_date`, `exchange_rate`, `source_currency`, `target_currency`, `version`, `unique_key`, `base_currency`, `date`, `rate`) VALUES
+(1, '2024-03-19', 0.91, 'USD', 'EUR', NULL, NULL, NULL, NULL, NULL),
+(2, '2024-03-19', 57.28, 'USD', 'PHP', NULL, NULL, NULL, NULL, NULL),
+(3, '2024-03-19', 1.09, 'EUR', 'USD', NULL, NULL, NULL, NULL, NULL),
+(4, '2024-03-19', 62.65, 'EUR', 'PHP', NULL, NULL, NULL, NULL, NULL),
+(5, '2024-03-19', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(6, '2024-03-19', 0.02, 'PHP', 'EUR', NULL, NULL, NULL, NULL, NULL),
+(7, '2025-03-19', 1.20, 'USD', 'EUR', NULL, NULL, NULL, NULL, NULL),
+(14, '2025-03-23', 1.09, 'USD', 'EUR', NULL, NULL, NULL, NULL, NULL),
+(15, '2025-03-10', 1.09, 'USD', 'PHP', NULL, NULL, NULL, NULL, NULL),
+(18, '2025-03-25', 0.92, 'USD', 'EUR', NULL, NULL, NULL, NULL, NULL),
+(26, '2025-03-08', 0.93, 'USD', 'EUR', NULL, NULL, NULL, NULL, NULL),
+(27, '2025-03-09', 0.93, 'USD', 'EUR', NULL, NULL, NULL, NULL, NULL),
+(28, '2025-03-09', 57.35, 'USD', 'PHP', NULL, NULL, NULL, NULL, NULL),
+(29, '2025-03-03', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(32, '2025-03-15', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(34, '2025-03-16', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(35, '2025-03-16', 0.02, 'PHP', 'EUR', NULL, NULL, NULL, NULL, NULL),
+(37, '2025-03-16', 2.62, 'PHP', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(38, '2025-03-16', 0.07, 'PHP', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(39, '2025-03-16', 3.75, 'USD', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(40, '2025-03-12', 3.75, 'USD', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(42, '2025-03-12', 0.07, 'PHP', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(43, '2025-05-12', 0.07, 'PHP', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(44, '2024-05-12', 0.07, 'PHP', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(45, '2024-04-12', 0.07, 'PHP', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(46, '2023-04-12', 0.07, 'PHP', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(47, '2023-04-19', 0.07, 'PHP', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(48, '2023-04-19', 2.62, 'PHP', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(49, '2023-04-19', 150.36, 'USD', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(50, '2024-04-19', 150.36, 'USD', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(51, '2025-04-19', 150.36, 'USD', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(53, '2025-03-19', 57.35, 'USD', 'PHP', NULL, NULL, NULL, NULL, NULL),
+(59, '2025-03-25', 57.35, 'USD', 'PHP', NULL, NULL, NULL, NULL, NULL),
+(61, '2025-04-17', 150.36, 'USD', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(62, '2025-02-17', 150.36, 'USD', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(65, '2025-02-17', 7.26, 'USD', 'CNY', NULL, NULL, NULL, NULL, NULL),
+(84, '2025-02-20', 7.26, 'USD', 'CNY', NULL, NULL, NULL, NULL, NULL),
+(85, '2025-02-21', 7.26, 'USD', 'CNY', NULL, NULL, NULL, NULL, NULL),
+(86, '2025-02-25', 7.26, 'USD', 'CNY', NULL, NULL, NULL, NULL, NULL),
+(87, '2025-02-25', 0.05, 'JPY', 'CNY', NULL, NULL, NULL, NULL, NULL),
+(88, '2025-02-25', 0.38, 'JPY', 'PHP', NULL, NULL, NULL, NULL, NULL),
+(89, '2025-02-25', 0.01, 'JPY', 'EUR', NULL, NULL, NULL, NULL, NULL),
+(90, '2025-02-25', 0.01, 'JPY', 'USD', NULL, NULL, NULL, NULL, NULL),
+(91, '2025-02-25', 0.14, 'CNY', 'USD', NULL, NULL, NULL, NULL, NULL),
+(92, '2025-02-25', 0.13, 'CNY', 'EUR', NULL, NULL, NULL, NULL, NULL),
+(93, '2025-02-25', 0.52, 'CNY', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(95, '2025-02-26', 0.52, 'CNY', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(96, '2025-02-27', 0.52, 'CNY', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(97, '2025-03-27', 0.52, 'CNY', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(98, '2025-03-27', 20.66, 'CNY', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(99, '2025-01-27', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(100, '2025-01-01', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(101, '2025-01-01', 2.62, 'PHP', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(104, '2025-01-02', 2.62, 'PHP', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(106, '2025-01-02', 0.07, 'PHP', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(111, '2025-01-03', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(115, '2025-01-04', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(117, '2025-01-05', 0.02, 'PHP', 'USD', 0, NULL, NULL, NULL, NULL),
+(121, '2025-01-06', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(130, '2025-01-07', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(131, '2025-01-09', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(135, '2025-01-10', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(138, '2025-01-11', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(142, '2025-01-12', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(150, '2025-01-16', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(162, '2025-03-19', 0.02, 'PHP', 'USD', NULL, NULL, NULL, NULL, NULL),
+(163, '2025-03-19', 2.61, 'PHP', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(164, '2025-03-19', 162.02, 'EUR', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(165, '2025-03-19', 39.98, 'SAR', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(172, '2025-03-20', 39.98, 'SAR', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(175, '2025-03-21', 39.98, 'SAR', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(177, '2025-03-21', 0.03, 'JPY', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(178, '2025-03-21', 4.05, 'EUR', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(179, '2025-03-27', 4.05, 'EUR', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(181, '2025-03-28', 4.05, 'EUR', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(184, '2025-03-24', 4.05, 'EUR', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(193, '2025-03-26', 4.05, 'EUR', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(195, '2025-03-26', 3.75, 'USD', 'SAR', NULL, NULL, NULL, NULL, NULL),
+(196, '2025-03-26', 149.94, 'USD', 'JPY', NULL, NULL, NULL, NULL, NULL),
+(201, '2025-03-26', 7.26, 'USD', 'CNY', NULL, NULL, NULL, NULL, NULL),
+(202, '2025-03-26', 24.00, 'USD', 'CUP', NULL, NULL, NULL, NULL, NULL),
+(203, '2025-03-26', 2.40, 'CUP', 'PHP', NULL, NULL, NULL, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `exchange_rates`
+--
+ALTER TABLE `exchange_rates`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UKifrgejrvnadfjr4bbp3frqm0b` (`cut_off_date`,`source_currency`,`target_currency`),
+  ADD UNIQUE KEY `UK1yj0uh15pqb6hit15oje6l072` (`cut_off_date`,`source_currency`,`target_currency`),
+  ADD UNIQUE KEY `UKtb05d1gnesl9hn8onk8q5i5xg` (`unique_key`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `exchange_rates`
+--
+ALTER TABLE `exchange_rates`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=205;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
